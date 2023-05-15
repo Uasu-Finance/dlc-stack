4c84b11dfa906a1ace4a3b98997790442f7d6714
sosaucily <sosaucily@gmail.com>
2023-05-15 13:47:38 +0200

    Updates to Oracle, use rust-dlc 0.4.0, remove most of sibyls custom logic

--- oracle/src/oracle/mod.rs
+++ oracle/src/oracle/mod.rs
@@ -1,7 +1,4 @@
-use crate::OracleConfig;
-use secp256k1_zkp::All;
-use secp256k1_zkp::KeyPair;
-use secp256k1_zkp::Secp256k1;
+use secp256k1_zkp::{All, KeyPair, Secp256k1, SecretKey};
 use serde::{Deserialize, Serialize};
 
 mod error;
@@ -13,7 +10,7 @@ pub use error::Result;
 #[derive(Clone, Deserialize, Serialize)]
 // outstanding_sk_nonces?, suredbits_announcement, suredbits_attestation?, announcement, attestation?, outcome?, uuid
 pub struct DbValue(
-    pub Option<Vec<[u8; 32]>>,
+    pub Option<Vec<SecretKey>>,
     pub Vec<u8>,
     pub Option<Vec<u8>>,
     pub Vec<u8>,
@@ -24,43 +21,30 @@ pub struct DbValue(
 
 #[derive(Clone)]
 pub struct Oracle {
-    pub oracle_config: OracleConfig,
     pub event_handler: EventHandler,
-    keypair: KeyPair,
-    secp: Secp256k1<All>,
+    pub key_pair: KeyPair,
+    pub secp: Secp256k1<All>,
 }
 
 impl Oracle {
-    pub fn new(
-        oracle_config: OracleConfig,
-        keypair: KeyPair,
-        secp: Secp256k1<All>,
-    ) -> Result<Oracle> {
-        if !oracle_config.announcement_offset.is_positive() {
-            return Err(OracleError::InvalidAnnouncementTimeError(
-                oracle_config.announcement_offset,
-            ));
-        }
+    pub fn new(key_pair: KeyPair, secp: Secp256k1<All>) -> Result<Oracle> {
         let event_handler = EventHandler::new();
 
         Ok(Oracle {
-            oracle_config,
             event_handler,
-            keypair,
+            key_pair,
             secp,
         })
     }
 
     pub fn get_keypair(&self) -> &KeyPair {
-        &self.keypair
+        &self.key_pair
     }
     pub fn get_secp(&self) -> &Secp256k1<All> {
         &self.secp
     }
 }
 
-pub mod oracle_queryable;
+// pub mod oracle_queryable;
 pub mod secret_key;
 pub mod vault;
-
-pub use oracle_queryable::messaging::EventDescriptor;
