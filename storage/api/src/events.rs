0485e53ecddf670ddabb904f559ec9e183e0f895
scolear <bobtera@gmail.com>
2023-07-10 11:10:27 +0200

    feat: add debug printing to storage-api

--- storage/api/src/events.rs
+++ storage/api/src/events.rs
@@ -4,18 +4,21 @@ use actix_web::{delete, get, post, put, HttpResponse, Responder};
 use dlc_storage_common::models::{NewEvent, UpdateEvent};
 use dlc_storage_reader;
 use dlc_storage_writer;
+use log::debug;
 
 #[get("/events")]
 pub async fn get_events(pool: Data<DbPool>) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
     let events = dlc_storage_reader::get_events(&mut conn).unwrap();
+    debug!("GET: /events : {:?}", events);
     HttpResponse::Ok().json(events)
 }
 
 #[get("/events/{uuid}")]
 pub async fn get_event(pool: Data<DbPool>, uuid: Path<String>) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
-    let result = dlc_storage_reader::get_event(&mut conn, &uuid.into_inner());
+    let result = dlc_storage_reader::get_event(&mut conn, &uuid.clone());
+    debug!("GET: /events/{} : {:?}", uuid, result);
     match result {
         Ok(events) => HttpResponse::Ok().json(events),
         Err(diesel::result::Error::NotFound) => HttpResponse::NotFound().body("Event not found"),
@@ -27,6 +30,7 @@ pub async fn get_event(pool: Data<DbPool>, uuid: Path<String>) -> impl Responder
 pub async fn create_event(pool: Data<DbPool>, event: Json<NewEvent>) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
     let events = dlc_storage_writer::create_event(&mut conn, event.into_inner()).unwrap();
+    debug!("POST: /events : {:?}", events);
     HttpResponse::Ok().json(events)
 }
 
@@ -38,15 +42,16 @@ pub async fn update_event(
 ) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
     let events =
-        dlc_storage_writer::update_event(&mut conn, &uuid.into_inner(), event.into_inner())
-            .unwrap();
+        dlc_storage_writer::update_event(&mut conn, &uuid.clone(), event.into_inner()).unwrap();
+    debug!("PUT: /events/{} : {:?}", uuid, events);
     HttpResponse::Ok().json(events)
 }
 
 #[delete("/events/{uuid}")]
 pub async fn delete_event(pool: Data<DbPool>, uuid: Path<String>) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
-    let num_deleted = dlc_storage_writer::delete_event(&mut conn, &uuid.into_inner()).unwrap();
+    let num_deleted = dlc_storage_writer::delete_event(&mut conn, &uuid.clone()).unwrap();
+    debug!("DELETE: /events/{} : {:?}", uuid, num_deleted);
     HttpResponse::Ok().json(num_deleted)
 }
 
@@ -54,5 +59,6 @@ pub async fn delete_event(pool: Data<DbPool>, uuid: Path<String>) -> impl Respon
 pub async fn delete_events(pool: Data<DbPool>) -> impl Responder {
     let mut conn = pool.get().expect("couldn't get db connection from pool");
     let num_deleted = dlc_storage_writer::delete_events(&mut conn).unwrap();
+    debug!("DELETE: /events : {:?}", num_deleted);
     HttpResponse::Ok().json(num_deleted)
 }
