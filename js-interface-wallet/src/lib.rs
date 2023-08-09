2f1a99b742f3ad7cafa435f591fa0f4b3f73978d
sosaucily <sosaucily@gmail.com>
2023-08-09 20:47:59 +0200

    clean up unused imports on js interface wwallet

--- js-interface-wallet/src/lib.rs
+++ js-interface-wallet/src/lib.rs
@@ -1,37 +1,13 @@
-use std::{ cell::RefCell, ops::Deref, str::FromStr };
+use std::{cell::RefCell, str::FromStr};
 
 use bdk::{
-    database::{ BatchOperations, Database },
-    wallet::coin_selection::{
-        decide_change,
-        BranchAndBoundCoinSelection,
-        CoinSelectionAlgorithm,
-        CoinSelectionResult,
-    },
-    FeeRate,
-    KeychainKind,
-    LocalUtxo,
-    Utxo as BdkUtxo,
-    WeightedUtxo,
+    wallet::coin_selection::{decide_change, CoinSelectionResult},
+    FeeRate, KeychainKind, LocalUtxo, Utxo as BdkUtxo, WeightedUtxo,
 };
-use bitcoin::{
-    hashes::Hash,
-    Address,
-    Network,
-    PackedLockTime,
-    PrivateKey,
-    Script,
-    Sequence,
-    Transaction,
-    TxIn,
-    TxOut,
-    Txid,
-    Witness,
-};
-use dlc_manager::{ error::Error, Blockchain, Signer, Utxo, Wallet };
-use lightning::chain::chaininterface::{ ConfirmationTarget, FeeEstimator };
-use rust_bitcoin_coin_selection::select_coins;
-use secp256k1_zkp::{ rand::thread_rng, All, PublicKey, Secp256k1, SecretKey };
+use bitcoin::{hashes::Hash, Address, Network, PrivateKey, Script, Txid};
+use dlc_manager::{error::Error, Blockchain, Signer, Utxo, Wallet};
+use lightning::chain::chaininterface::FeeEstimator;
+use secp256k1_zkp::{All, PublicKey, Secp256k1, SecretKey};
 type Result<T> = core::result::Result<T, Error>;
 
 pub(crate) const TXIN_BASE_WEIGHT: usize = (32 + 4 + 4) * 4;
@@ -42,32 +18,6 @@ pub trait WalletBlockchainProvider: Blockchain + FeeEstimator {
     fn is_output_spent(&self, txid: &Txid, vout: u32) -> Result<bool>;
 }
 
-/// Trait enabling the wallet to persist data.
-// pub trait WalletStorage {
-//     fn upsert_address(&self, address: &Address, privkey: &SecretKey) -> Result<()>;
-//     fn delete_address(&self, address: &Address) -> Result<()>;
-//     fn get_addresses(&self) -> Result<Vec<Address>>;
-//     fn get_priv_key_for_address(&self, address: &Address) -> Result<Option<SecretKey>>;
-//     fn upsert_key_pair(&self, public_key: &PublicKey, privkey: &SecretKey) -> Result<()>;
-//     fn get_priv_key_for_pubkey(&self, public_key: &PublicKey) -> Result<Option<SecretKey>>;
-//     fn upsert_utxo(&self, utxo: &Utxo) -> Result<()>;
-//     fn has_utxo(&self, utxo: &Utxo) -> Result<bool>;
-//     fn delete_utxo(&self, utxo: &Utxo) -> Result<()>;
-//     fn get_utxos(&self) -> Result<Vec<Utxo>>;
-//     fn unreserve_utxo(&self, txid: &Txid, vout: u32) -> Result<()>;
-// }
-
-/// Basic wallet mainly meant for testing purposes.
-// pub struct JSInterfaceWallet<B: Deref, W: Deref>
-// where
-//     B::Target: WalletBlockchainProvider,
-//     W::Target: WalletStorage,
-// {
-//     blockchain: B,
-//     storage: W,
-//     secp_ctx: Secp256k1<All>,
-//     network: Network,
-// }
 pub struct JSInterfaceWallet {
     address: Address,
     secp_ctx: Secp256k1<All>,
@@ -78,8 +28,6 @@ pub struct JSInterfaceWallet {
 
 impl JSInterfaceWallet {
     pub fn new(address_str: String, network: Network, privkey: PrivateKey) -> Self {
-        // let secp_ctx = Secp256k1::new();
-
         Self {
             address: Address::from_str(&address_str).unwrap(),
             // address: Address::p2wpkh(
@@ -99,20 +47,6 @@ impl JSInterfaceWallet {
         self.utxos.borrow_mut().as_mut().unwrap().append(&mut utxos);
         Ok(())
     }
-
-    // where
-    //     B::Target: WalletBlockchainProvider,
-    //     W::Target: WalletStorage,
-    // {
-    //     /// Create a new wallet instance.
-    //     pub fn new(blockchain: B, storage: W, network: Network) -> Self {
-    //         Self {
-    //             blockchain,
-    //             storage,
-    //             secp_ctx: Secp256k1::new(),
-    //             network,
-    //         }
-    //     }
 
     // Refresh the wallet checking and updating the UTXO states.
     // pub fn refresh(&self) -> Result<()> {
@@ -219,7 +153,7 @@ impl Signer for JSInterfaceWallet {
         tx: &mut bitcoin::Transaction,
         input_index: usize,
         tx_out: &bitcoin::TxOut,
-        _: Option<bitcoin::Script>
+        _: Option<bitcoin::Script>,
     ) -> Result<()> {
         // let address = Address::from_script(&tx_out.script_pubkey, self.network)
         //     .expect("a valid scriptpubkey");
@@ -233,7 +167,7 @@ impl Signer for JSInterfaceWallet {
             tx,
             input_index,
             bitcoin::EcdsaSighashType::All,
-            tx_out.value
+            tx_out.value,
         )?;
         Ok(())
     }
@@ -247,7 +181,7 @@ fn select_sorted_utxos(
     utxos: impl Iterator<Item = (bool, WeightedUtxo)>,
     fee_rate: FeeRate,
     target_amount: u64,
-    drain_script: &Script
+    drain_script: &Script,
 ) -> Result<CoinSelectionResult> {
     let mut selected_amount = 0;
     let mut fee_amount = 0;
@@ -256,16 +190,15 @@ fn select_sorted_utxos(
             (&mut selected_amount, &mut fee_amount),
             |(selected_amount, fee_amount), (must_use, weighted_utxo)| {
                 if must_use || **selected_amount < target_amount + **fee_amount {
-                    **fee_amount += fee_rate.fee_wu(
-                        TXIN_BASE_WEIGHT + weighted_utxo.satisfaction_weight
-                    );
+                    **fee_amount +=
+                        fee_rate.fee_wu(TXIN_BASE_WEIGHT + weighted_utxo.satisfaction_weight);
                     **selected_amount += weighted_utxo.utxo.txout().value;
 
                     Some(weighted_utxo.utxo)
                 } else {
                     None
                 }
-            }
+            },
         )
         .collect::<Vec<_>>();
 
@@ -298,7 +231,7 @@ impl Wallet for JSInterfaceWallet {
         &self,
         amount: u64,
         fee_rate: Option<u64>,
-        lock_utxos: bool
+        lock_utxos: bool,
     ) -> Result<Vec<Utxo>> {
         let org_utxos = self.utxos.borrow().as_ref().unwrap().clone();
         let mut utxos = org_utxos
@@ -315,10 +248,11 @@ impl Wallet for JSInterfaceWallet {
             })
             .collect::<Vec<_>>();
         let dummy_pubkey: PublicKey =
-            "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798".parse().unwrap();
-        let dummy_drain = Script::new_v0_p2wpkh(
-            &bitcoin::WPubkeyHash::hash(&dummy_pubkey.serialize())
-        );
+            "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"
+                .parse()
+                .unwrap();
+        let dummy_drain =
+            Script::new_v0_p2wpkh(&bitcoin::WPubkeyHash::hash(&dummy_pubkey.serialize()));
         let fee_rate = FeeRate::from_sat_per_vb(fee_rate.unwrap() as f32);
         let required_utxos = Vec::new();
         let drain_script = &dummy_drain;
@@ -328,12 +262,7 @@ impl Wallet for JSInterfaceWallet {
             required_utxos
                 .into_iter()
                 .map(|utxo| (true, utxo))
-                .chain(
-                    utxos
-                        .into_iter()
-                        .rev()
-                        .map(|utxo| (false, utxo))
-                )
+                .chain(utxos.into_iter().rev().map(|utxo| (false, utxo)))
         };
 
         let selection = select_sorted_utxos(temp_utxos, fee_rate, amount, drain_script).unwrap();
@@ -370,31 +299,3 @@ impl rust_bitcoin_coin_selection::Utxo for UtxoWrap {
         self.utxo.tx_out.value
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use std::rc::Rc;
-
-//     use dlc_manager::{Signer, Wallet};
-//     use mocks::simple_wallet::JSInterfaceWallet;
-//     use mocks::{memory_storage_provider::MemoryStorage, mock_blockchain::MockBlockchain};
-//     use secp256k1_zkp::{PublicKey, SECP256K1};
-
-//     fn get_wallet() -> JSInterfaceWallet<Rc<MockBlockchain>, Rc<MemoryStorage>> {
-//         let blockchain = Rc::new(MockBlockchain {});
-//         let storage = Rc::new(MemoryStorage::new());
-//         let wallet = JSInterfaceWallet::new(blockchain, storage, bitcoin::Network::Regtest);
-//         wallet
-//     }
-
-//     #[test]
-//     fn get_new_secret_key_can_be_retrieved() {
-//         let wallet = get_wallet();
-//         let sk = wallet.get_new_secret_key().unwrap();
-//         let pk = PublicKey::from_secret_key(SECP256K1, &sk);
-
-//         let sk2 = wallet.get_secret_key_for_pubkey(&pk).unwrap();
-
-//         assert_eq!(sk, sk2);
-//     }
-// }
